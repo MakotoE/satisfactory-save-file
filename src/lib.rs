5f5e4a51d4f53d17b13a64e50783186b1ec15fee
Makoto <makoto@emurasoft.com>
2021-05-15 15:43:35 -0700

    Add first part of save file

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 use anyhow::Result;
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian as L, ReadBytesExt};
 use std::io::Read;
 
 #[derive(Debug, Default, Clone)]
 pub struct SaveFile {
-    save_header: i32,
+    pub save_header: i32,
+    pub save_version: i32,
+    pub build_version: i32,
+    pub world_type: String,       // Make this an enum
+    pub world_properties: String, // Make this a struct
+    pub play_time: i32,           // Make this a duration type
+    pub save_date: i64,           // Make this a date type
+    pub session_visibility: u8,   // Make this an enum
 }
 
 impl SaveFile {
@@ -23,7 +30,31 @@ impl SaveFile {
     {
         // https://satisfactory.fandom.com/wiki/Save_files
 
-        self.save_header = file.read_i32::<LittleEndian>()?;
+        let mut buffer: Vec<u8> = Vec::new();
+
+        self.save_header = file.read_i32::<L>()?;
+        self.save_version = file.read_i32::<L>()?;
+        self.build_version = file.read_i32::<L>()?;
+        SaveFile::read_string(file, &mut buffer, &mut self.world_type);
+        SaveFile::read_string(file, &mut buffer, &mut self.world_properties);
+        self.play_time = file.read_i32::<L>()?;
+        self.save_date = file.read_i64::<L>()?;
+        self.session_visibility = file.read_u8()?;
+        Ok(())
+    }
+
+    fn read_string<R>(file: &mut R, buffer: &mut Vec<u8>, s: &mut String) -> Result<()>
+    where
+        R: Read,
+    {
+        let length = (file.read_i32::<L>()? - 1) as usize; // This can be negative
+        buffer.clear();
+        buffer.resize(length, b'\0');
+        file.read_exact(buffer);
+        // Skip null char
+        file.read_u8()?;
+        s.clear();
+        s.push_str(std::str::from_utf8(&buffer)?);
         Ok(())
     }
 }
@@ -31,7 +62,7 @@ impl SaveFile {
 #[cfg(test)]
 mod tests {
     use crate::SaveFile;
-    use std::fs::{read, File};
+    use std::fs::File;
 
     #[test]
     fn parse() {
