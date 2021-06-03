8cbab0d33ed5a81981c6d07c1864ad1c42659b9a
Makoto <makoto@emurasoft.com>
2021-06-03 16:55:46 -0700

    fix test

--- fuzz/fuzz_targets/save_file.rs
+++ fuzz/fuzz_targets/save_file.rs
@@ -1,7 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use satisfactory_save_file::SaveFile;
+use std::io::Cursor;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = SaveFile::parse(&mut &data.to_vec()[..]);
+    let _ = SaveFile::parse(&mut Cursor::new(data));
 });
