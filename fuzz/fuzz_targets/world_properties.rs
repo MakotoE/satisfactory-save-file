cef116989aaf8c9970e38fa300a2e6d2370b423b
Makoto <makoto@emurasoft.com>
2021-06-03 16:09:57 -0700

    add fuzz test

---
+++ fuzz/fuzz_targets/world_properties.rs
@@ -0,0 +1,9 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use satisfactory_save_file::WorldProperties;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = WorldProperties::new(s);
+    }
+});
